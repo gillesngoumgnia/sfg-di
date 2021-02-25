135e473252c29c260b23df63672cea660b3f771e
gilles <gillesngoumgnia@yahoo.fr>
2021-02-25 10:20:57 +0100

    Feat: Dependency Injection without Spring

--- src/test/java/guru/springframework/sfgdi/SfgDiApplicationTests.java
+++ src/test/java/guru/springframework/sfgdi/SfgDiApplicationTests.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controller.PropertyInjectedController;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
